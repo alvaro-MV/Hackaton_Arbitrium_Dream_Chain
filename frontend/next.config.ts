f2749a10f8e6c912b867d36fea623b3124401a06
Álvaro Morales <moralesalvaro97@gmail.com>
2025-03-22 00:35:12 +0100

    disable eslint on builds to test build faster

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
 	reactStrictMode: true,
 	swcMinify: true,
+	eslint: {
+		ignoreDuringBuilds: true,
+	},
 
 	webpack: (config, { isServer }) => {
 		if (!isServer) {
