99cbeeff8a6b39d463ee9f9d36a63b231affa4b8
Álvaro Morales <moralesalvaro97@gmail.com>
2025-03-22 00:39:45 +0100

    Fix typescript error

--- frontend/app/Web3Provider.tsx
+++ frontend/app/Web3Provider.tsx
@@ -1,12 +1,13 @@
 "use client"
 
 import "@rainbow-me/rainbowkit/styles.css"
-import type { PropsWithChildren } from "react"
+import { type ReactNode } from "react"
 
 import { getDefaultConfig, RainbowKitProvider } from "@rainbow-me/rainbowkit"
 import { WagmiProvider } from "wagmi"
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import { arbitrumSepolia } from "viem/chains"
+import React from "react"
 
 const config = getDefaultConfig({
   appName: "42TemplateEarnHack",
@@ -17,7 +18,7 @@ const config = getDefaultConfig({
 
 const queryClient = new QueryClient()
 
-export default function Web3Provider({ children }: PropsWithChildren) {
+export default function Web3Provider({ children }: {children: any}) {
   return (
     <WagmiProvider config={config}>
       <QueryClientProvider client={queryClient}>
